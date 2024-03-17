c7034d7e00157a214c4c39b9ecf2ab87a3ac8e11
KartikPatil20 <kartikpatilcse@gmail.com>
2024-03-17 13:41:37 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
+const customDictionary = {
+
+  teh: "the",
+
+  wrok: "work",
+
+  fot: "for",
+
+  exampl: "example"
+
+};
+
+
 function App() {
+
+  const [inputText, setInputText] = useState("");
+  const [correction, setCorrection] = useState(null);
+
+  const HandleInput = (text) => {
+    setInputText(text);
+    Checkspell(text);
+  }
+
+
+  const Checkspell = (text) => {
+    for(const[incorrectWord, correctWord] of Object.entries(customDictionary)){
+    
+      if(text.match(incorrectWord)){
+        setCorrection(`${correctWord}?`);
+        return;
+      }
+    }
+    setCorrection(null);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Spell Check and Auto-Correction</h1>
+      <textarea placeholder='Enter text...' value={inputText} onChange={(e)=>HandleInput(e.target.value)} />
+      { correction && <p>Did you mean: <span className='correct_word'>{correction}</span></p>}
     </div>
   );
 }
